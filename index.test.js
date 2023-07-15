4382ca26942cf40ac62380d5b2b4df3974f91a9a
gozde-apkk <gozdeapak.ga.1@gmail.com>
2023-07-15 15:37:27 +0300

    all missions done

--- index.test.js
+++ index.test.js
@@ -31,7 +31,7 @@ describe('[Görev 3] enBuyukTamsayiyiBul', () => {
   const tamsayilar  =[{ tamsayi: 1 }, { tamsayi: 3 }, { tamsayi: 2 }]
    test('[5] bir dizi nesne içindeki en büyük tamsayiyi döndürüyor { tamsayi: 2 }', () => {
     const max = utils.enBuyukTamsayiyiBul(tamsayilar);
-    expect(max).toBe(5);
+    expect(max).toBe(3);
    })
 })
 
@@ -62,90 +62,80 @@ describe('[Görev 4] Sayici', () => {
 describe('[Görev 5] Mevsimler', () => {
   let mevsimler
   beforeEach(() => {
-    //OBJE YARATIYOR
     mevsimler = new utils.Mevsimler() // her test yeni bir mevsimle başlar
   })
   test('[9] mevsimler.sonraki İLK çağırılışında "yaz" döndürüyor', () => {
-    let mevsim = mevsimler.sonraki();
-    expect(mevsim).toBe('yaz')
+    let expected = mevsimler.sonraki();
+    expect(expected).toBe("yaz");
   })
-   test('[10] mevsimler.sonraki İKİNCİ çağırılışında "sonbahar" döndürüyor', () => {
-    let mevsim = mevsimler.sonraki();
-    mevsim = mevsim.sonraki();
-    expect(mevsim).toBe('sonbahar');
-   })
+  test('[10] mevsimler.sonraki İKİNCİ çağırılışında "sonbahar" döndürüyor', () => {
+    let expected = mevsimler.sonraki();
+    expected = mevsimler.sonraki();
+    expect(expected).toBe("sonbahar");
+  })
   test('[11] mevsimler.sonraki ÜÇÜNCÜ çağırılışında "kış" döndürüyor', () => {
-    let mevsim = mevsimler.sonraki();
-    mevsim = mevsim.sonraki();
-    mevsim = mevsim.sonraki();
-    expect(mevsim).toBe('kış');
+    let expected = "";
+    for (let i = 1; i <= 3; i++) {
+      expected = mevsimler.sonraki()
+    };
+    expect(expected).toBe("kış");
   })
-   test('[12] mevsimler.sonraki DÖRDÜNCÜ çağırılışında "ilkbahar" döndürüyor', () => {})
-   let mevsim = mevsimler.sonraki();
-   mevsim = mevsim.sonraki();
-   mevsim = mevsim.sonraki();
-   mevsim = mevsim.sonraki();
-   expect(mevsim).toBe('ilkbahar');
-   test('[13] mevsimler.sonraki BEŞİNCİ çağırılışında "yaz" döndürüyor', () => {
-    for (let i = 0; i < 4; i++) {
-      mevsimler.sonraki();
-    }
-
-    
-   expect(mevsim).toBe('yaz');
-   })
-   test('[14] mevsimler.sonraki KIRKINCI çağırılışında "ilkbahar" döndürüyor', () => {
-    let mevsim = mevsimler.sonraki();
-    for (let i=0; i<39; i++){
-      mevsim = mevsimler.sonraki();
-    }
-    expect(mevsim).toBe('ilkbahar');
-   })
+  test('[12] mevsimler.sonraki DÖRDÜNCÜ çağırılışında "ilkbahar" döndürüyor', () => {
+    let expected = "";
+    for (let i = 1; i <= 4; i++) {
+      expected = mevsimler.sonraki()
+    };
+    expect(expected).toBe("ilkbahar");
+  })
+  test('[13] mevsimler.sonraki BEŞİNCİ çağırılışında "yaz" döndürüyor', () => {
+    let expected = "";
+    for (let i = 1; i <= 5; i++) {
+      expected = mevsimler.sonraki()
+    };
+    expect(expected).toBe("yaz");
+  })
+  test('[14] mevsimler.sonraki KIRKINCI çağırılışında "ilkbahar" döndürüyor', () => {
+    let expected = "";
+    for (let i = 1; i <= 40; i++) {
+      expected = mevsimler.sonraki()
+    };
+    expect(expected).toBe("ilkbahar");
+  })
 })
-
 describe('[Görev 6] Araba', () => {
   let focus
   beforeEach(() => {
     focus = new utils.Araba('focus', 20, 30) // her test yeni bir araba oluşturur
   })
-   test('[15] arabayı sürünce güncellenmiş odometer döndürüyor', () => {
-    let total = focus.sur(100);
-    total = focus.sur(100);
-    total = focus.sur(100);
-    total = focus.sur(200);
-    total = focus.sur(200);
-    expect(total).toBe(600);
-    
-   })
- test('[16] arabayı sürmek benzin tüketiyor', () => {
-  let total = focus.sur(100);
-    total = focus.sur(100);
-    total = focus.sur(100);
-    total = focus.sur(200);
-    total = focus.sur(200);
-    expect(focus.depo).toBe(0);
- })
-  test('[17] benzinalma arabayı sürmeye izin veriyor', () => {
-    let total = focus.sur(100);
-    total = focus.sur(100);
-    total = focus.sur(100);
-    total = focus.sur(200);
-    total = focus.sur(200);
-    focus.benzinal(10);
-    total = focus.sur(200);
-    expect(total).toBe(800);
+  test('[15] arabayı sürünce güncellenmiş odometer döndürüyor', () => {
+    focus.sur(100);
+    let actual = focus.sur(100);
+    expect(actual).toBe(200)
   })
-   test('[18] dolu depoya benzin alma etki etmiyor', () => {
-    let total = focus.sur(100);
-    total = focus.sur(100);
-    total = focus.sur(100);
-    total = focus.sur(200);
-    total = focus.sur(200);
-     focus.benzinal(20);
-    expect(depo).toBe(20);
-    depo = focus.benzinal(20)
-    expect(depo).toBe(20);
-   })
+  test('[16] arabayı sürmek benzin tüketiyor', () => {
+    focus.sur(100)
+    focus.sur(100)
+    focus.sur(100)
+    expect(focus.depo).toBe(11);
+  })
+  test('[17] benzinalma arabayı sürmeye izin veriyor', () => {
+    focus.sur(600)
+    focus.benzinal(20)
+    focus.sur(600)
+    focus.benzinal(20)
+    focus.sur(600)
+    focus.benzinal(20)
+    let actual = focus.sur(600)
+    expect(actual).toBe(600)
+  })
+  test('[18] dolu depoya benzin alma etki etmiyor', () => {
+    focus.benzinal(5)
+    focus.benzinal(5)
+    focus.benzinal(5)
+    focus.benzinal(5)
+    let actual = focus.benzinal(5)
+    expect(focus.depo).toBe(20)
+  })
 })
 
 describe('[Görev 7] asenkronCiftSayi', () => {
